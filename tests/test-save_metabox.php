3ad984da1b5df598fdea8bf9127129b62d57fc42
keesiemeijer <keesie.meijer@gmail.com>
2015-06-06 12:55:12 +0200

    Removed the functions that moved to the new Test_Utils class. Updated the test to use the new class functions

--- tests/test-save_metabox.php
+++ tests/test-save_metabox.php
@@ -4,17 +4,15 @@ use \WP_UnitTestCase;
 
 class Save_Metabox_Tests extends WP_UnitTestCase {
 
-	private $additional1 = array(
-		'additional_content' => 'Hello',
-		'append'             => 'on',
-		'priority'           => 12,
-	);
+	/**
+	 * Set up.
+	 */
+	function setUp() {
+		parent::setUp();
 
-	private $additional2 = array(
-		'additional_content' => 'World!',
-		'prepend'            => 'on',
-		'priority'           => 10,
-	);
+		// Use the utils class to create posts with terms.
+		$this->utils = new Test_Utils( $this->factory );
+	}
 
 
 	public static function setUpBeforeClass() {
@@ -23,45 +21,20 @@ class Save_Metabox_Tests extends WP_UnitTestCase {
 	}
 
 
-	function setup_post() {
-
-		// set current user
-		$user = $this->factory->user->create( array( 'role' => 'administrator' ) );
-		wp_set_current_user( $user );
-
-		// includes the metaboxes.php file
-		metabox_includes();
-
-		// create a post
-		$post_id = $this->factory->post->create();
-
-		return $post_id;
-	}
-
-
-	function get_post_data( $post_id ) {
-		return array(
-			'post_title' => 'Post title',
-			'content' => 'Post content',
-			'post_ID' => $post_id,
-			'post_type' => 'post',
-			'ac_additional_content_nonce' => wp_create_nonce( 'ac_additional_content_nonce' ),
-		);
-	}
-
-
 	function test_empty_additional_content() {
 
-		$post_id = $this->setup_post();
-		$post_data = $this->get_post_data( $post_id );
+		$this->utils->setup_post_screen();
+
+		$post_id = $this->factory->post->create();
+		$post_data = $this->utils->get_post_data( $post_id );
 
 		// set additional post meta
-		update_post_meta( $post_id, '_ac_additional_content', array( $this->additional1 ) );
-		
+		update_post_meta( $post_id, '_ac_additional_content', array( $this->utils->additional1 ) );
+
 		// remove additional content.
-		$this->additional1['additional_content'] = '';
-		$post_data['ac_additional_content'][] = $this->additional1;
-		
+		$this->utils->additional1['additional_content'] = '';
+		$post_data['ac_additional_content'][] = $this->utils->additional1;
+
 		$_POST = _wp_translate_postdata( false, $post_data );
 
 		// saves the additional content metabox data for the post
@@ -75,15 +48,17 @@ class Save_Metabox_Tests extends WP_UnitTestCase {
 
 	function test_wrong_format_additional_content() {
 
-		$post_id = $this->setup_post();
-		$post_data = $this->get_post_data( $post_id );
+		$this->utils->setup_post_screen();
+
+		$post_id = $this->factory->post->create();
+		$post_data = $this->utils->get_post_data( $post_id );
 
 		// set additional post meta with correct format
-		update_post_meta( $post_id, '_ac_additional_content', array( $this->additional1 ) );
-		
+		update_post_meta( $post_id, '_ac_additional_content', array( $this->utils->additional1 ) );
+
 		// Wrong format
 		$post_data['ac_additional_content'] = 'string';
-		
+
 		$_POST = _wp_translate_postdata( false, $post_data );
 
 		// saves the additional content metabox data for the post
@@ -97,11 +72,17 @@ class Save_Metabox_Tests extends WP_UnitTestCase {
 
 	function test_order_by_priority() {
 
-		$post_id = $this->setup_post();
-		$post_data = $this->get_post_data( $post_id );
+		$this->utils->setup_post_screen();
 
-		$post_data['ac_additional_content'][] = $this->additional1;
-		$post_data['ac_additional_content'][] = $this->additional2;
+		$post_id = $this->factory->post->create();
+		$post_data = $this->utils->get_post_data( $post_id );
+
+		$additional1 = $this->utils->additional1;
+		$additional2 = $this->utils->additional2;
+
+
+		$post_data['ac_additional_content'][] = $additional1;
+		$post_data['ac_additional_content'][] = $additional2;
 
 		$_POST = _wp_translate_postdata( false, $post_data );
 
@@ -110,11 +91,11 @@ class Save_Metabox_Tests extends WP_UnitTestCase {
 
 		$additional = get_post_meta( $post_id, '_ac_additional_content', true );
 
-		$this->additional1['prepend'] = '';
-		$this->additional2['append']  = '';
+		$additional1['prepend'] = '';
+		$additional2['append']  = '';
 
 		// additional2 has a lower priority and should come first
-		$expected = array( $this->additional2, $this->additional1 );
+		$expected = array( $additional2, $additional1 );
 
 		$this->assertNotEmpty( $additional );
 		$this->assertEquals( $expected, $additional );
