ecf9ba5b92e9ba156d28bca54c43a2929cce73e0
keesiemeijer <keesie.meijer@gmail.com>
2015-10-19 16:51:04 +0200

    version 1.1.4

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit7ac843f69674cc7ee3cb0fde55e4402b::getLoader();
+return ComposerAutoloaderInit0faea6fcd858bd649f02e178462a44a4::getLoader();
